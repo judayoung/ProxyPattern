a3cd81bb0dbc4266f1d7ff950b84184231c3aa9f
Ju Dayoung <ekdud7703@naver.com>
2022-10-29 11:20:03 +0900

    수정 : ArrayIndexOutOfBoundsException 해결. 진행중 : 추후 Virtual Proxy, Remote Proxy 만들기.

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,20 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Let's see how Proxy works!\n");
 
-        Image image3 = new ImageProxy("image3.png");
-        image3.displayImage();
+        System.out.println("1. Virtual Proxy");
+        // 진행중 : 추후
+        System.out.println();
 
-//        System.out.println("1. Virtual Proxy");
-//        System.out.println("2. Remote Proxy");
-//        System.out.println("3. Protection Proxy");
+        System.out.println("2. Remote Proxy");
+        // 진행중 : 추후
+        System.out.println();
+
+        System.out.println("3. Protection Proxy");
+        String clientIp = "101.109.4.56";
+        String localhostIp = "127.0.0.1";
+        Image image3 = new ImageProxy("image3.png", clientIp);
+        image3.displayImage();
+        Image image4 = new ImageProxy("image3.png", localhostIp);
+        image4.displayImage();
     }
 }
--- src/proxy/ImageProxy.java
+++ src/proxy/ImageProxy.java
@@ -1,9 +1,15 @@
 package proxy;
 
-public class ImageProxy implements Image{
+public class ImageProxy implements Image {
 
     private ImageImpl imageImpl;
     private String fileName;
+    private String clientIp;
+
+    public ImageProxy(String fileName, String clientIp) {
+        this.fileName = fileName;
+        this.clientIp = clientIp;
+    }
 
     public ImageProxy(String fileName) {
         this.fileName = fileName;
@@ -11,8 +17,29 @@ public class ImageProxy implements Image{
 
     @Override
     public void displayImage() {
-        if(imageImpl == null) imageImpl = new ImageImpl(fileName);
+        if (isBlocked(clientIp)) {
+            printIsBlocked();
+            return;
+        }
+
+        if (imageImpl == null) imageImpl = new ImageImpl(fileName);
         imageImpl.displayImage();
+    }
+
+    private boolean isBlocked(String ip) {
+        /*
+         * . 으로 나누고 싶었는데, 마침 정규식에서 . 이 모든 문자를 의미해서 ㅠㅠ
+         * ArrayIndexOutOfBoundsException : Index 0 out of bounds for length 0
+         * 에러가 발생했다.
+         */
+        String[] ipArray = ip.split("[.]");
+        String firstIp = ipArray[0];
+        if (firstIp.equals("127")) return false;
+        else return true;
+    }
+
+    private void printIsBlocked() {
+        System.out.println("해당 ip(" + clientIp + ")는 이미지를 사용할 수 없습니다.");
     }
 
 }
